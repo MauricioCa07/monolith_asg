1d449f54bfa5567f9864d25ed4d0e2e559b6d3c0
MauricioCa07 <carrillomauricio007@gmail.com>
2025-05-16 13:31:41 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://database-1:admin123456789@database-1.cbdlmmsx8z7q.us-east-1.rds.amazonaws.com:3306/bookstore'
+SECRET_KEY = 'secretkey'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
